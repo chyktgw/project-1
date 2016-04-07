5c40690ca9ec7c33d3c178b11f93ac4ddb3d998f
chyktgw <chiyuki.x3@gmail.com>
2016-04-07 14:39:00 -0700

    Ready to Code

---
+++ server.js
@@ -0,0 +1,8 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+// var express = require('express');
+// var bodyParser = require('body-parser');
+
+// generate a new express app and call it 'app'
+// var app = express();

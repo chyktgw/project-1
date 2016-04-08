55537e224993b4a10ff2a118d79fc444fda89e4d
chyktgw <chiyuki.x3@gmail.com>
2016-04-08 11:10:26 -0700

    WIP server, index

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // SERVER-SIDE JAVASCRIPT
 
 //require express in our app
-// var express = require('express');
-// var bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 // generate a new express app and call it 'app'
-// var app = express();
+var app = express();
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});

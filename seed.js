e8f137e95446a00048bba81074e9858feca5cbc0
chyktgw <chiyuki.x3@gmail.com>
2016-04-08 10:40:27 -0700

    seed WIP

--- seed.js
+++ seed.js
@@ -1,1 +1,24 @@
-// var db = require("./models");
+var db = require("./models");
+
+
+var restaurantList =[];
+restaurantList.push({
+  name: 'Elephant Sushi',
+  address: '705 Geary St',
+  phoneNum: '415-555-1234',
+});
+restaurantList.push({
+  name: 'Restaurant B',
+  address: '355 Bush St',
+  phoneNum: '415-555-5555',
+});
+restaurantList.push({
+  name: 'Restaurant C',
+  address: '555 Market St',
+  phoneNum: '415-555-0412',
+});
+restaurantList.push({
+  name: 'Restaurant D',
+  address: '333 Larkin St',
+  phoneNum: '415-555-1111',
+});
